872d564785014f4734ce9933e26a6872e363d78d
GideonAdonteng <adontenggideon57@gmail.com>
2025-08-21 06:34:48 -1200

    updates

--- src/app/api/auth/kinde_login/route.ts
+++
@@ -1,7 +0,0 @@
-import { getKindeServerSession } from "@/lib/kinde";
-import { NextRequest } from "next/server";
-
-export async function GET(request: NextRequest) {
-  const { login } = await getKindeServerSession();
-  return login(request);
-}
